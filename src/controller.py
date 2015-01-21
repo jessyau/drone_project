4b505ce0768e85a4fd2c66aa799c7f106103bb61
viki <viki@c3po.(none)>
2015-01-21 14:03:34 -0800

    First commit to drone project

---
+++ src/controller.py
@@ -0,0 +1,15 @@
+# import roslib; roslib.load_manifest('drone_project')
+# import rospy
+# import rosbag
+# from geometry_msgs.msg import Twist
+# from std_msgs.msg import Empty
+# from std_msgs.msg import Float64
+
+# import sys, select, termios, tty
+# import time
+
+
+if __name__=="__main__":
+	key = raw_input("this: ")
+
+	print(key)
